a0225858d84d095a6b31bb29b8bb97f52b62b074
Vedant Chainani <vedantchainani1084@gmail.com>
2026-01-31 02:23:56 +0530

    chore: update packages and remove unused code

--- apps/web-vite/src/providers/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-import { QueryProvider } from "./query";
-
-export const ProviderTree = ({ children }: PropsWithChildren) => {
-  return <QueryProvider>{children}</QueryProvider>;
-};
