585c9f5e52d0cde6775838841de4cc275cacd8d6
Vedant Chainani <vedantchainani1084@gmail.com>
2026-01-31 02:47:48 +0530

    chore: add api template

---
+++ packages/api/src/index.ts
@@ -0,0 +1,12 @@
+import { HttpApi, OpenApi } from "@effect/platform";
+
+import { healthGroup } from "./health";
+
+export const api = HttpApi.make("YellowRpcAPI")
+  .add(healthGroup)
+  .annotate(OpenApi.Title, "YellowRPC API")
+  .annotate(OpenApi.Description, "YellowRPC API")
+  .annotate(OpenApi.License, {
+    name: "MIT",
+    url: "https://opensource.org/licenses/MIT",
+  });
