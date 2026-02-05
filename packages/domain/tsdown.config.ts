a131ebe1e18beeb82666a4c45c8f45b795aa6e31
Vedant Chainani <vedantchainani1084@gmail.com>
2026-02-05 19:43:12 +0530

    chore: remove build for internal packages

--- packages/domain/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { createTsdownConfig } from "@yellow-rpc/config/tsdown";
-
-export default createTsdownConfig({
-  entry: {
-    apiKey: "./src/api-key/index.ts",
-    helpers: "./src/helpers/index.ts",
-    session: "./src/session/index.ts",
-  },
-});
